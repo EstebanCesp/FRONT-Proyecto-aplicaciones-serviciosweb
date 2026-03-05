660f8bfc3e7fbdf452334c18c6f42c88d6ec2fea
EstebanCesp <estebancespedes1127042@correo.itm.edu.co>
2026-03-05 18:31:33 -0500

    Creacion del componente aliados

---
+++ src/app/components/aliados/aliados.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aliados',
+  imports: [],
+  templateUrl: './aliados.html',
+  styleUrl: './aliados.css',
+})
+export class Aliados {}
