ebb815965ea9ddf0903fe84c1cb216c25021c7be
David Arrieta <davidarrieta1212@gmail.com>
2026-03-05 23:34:08 -0500

    feat: archivo beca.service en services

--- src/app/services/beca.service.ts
+++ src/app/services/beca.service.ts
@@ -1,6 +1,29 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { BecaCreate, BecaUpdate } from '../models/Beca';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Beca {}
+export class BecaService {
+
+  private readonly endpoint = '/becas' 
+
+  constructor(private apiService: ApiService) {}
+
+  getBecas() {
+    return this.apiService.get(this.endpoint)
+  }
+
+  postBeca(data: BecaCreate) {
+    return this.apiService.post(this.endpoint, data)
+  }
+
+  putBeca(data: BecaUpdate, identificador: number) {
+  return this.apiService.put(this.endpoint, 'estudios', String(identificador), data)
+}
+
+deleteBeca(identificador: number) {
+  return this.apiService.delete(this.endpoint, 'estudios', String(identificador))
+}
+}
