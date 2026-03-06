4f1f929af532683697a7e2a891c28e9288e568ae
CarolinaG <carostudy21@gmail.com>
2026-03-06 03:36:43 -0500

    Creación del componente de apoyo profesoral

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
+import { ApoyoProfesoral } from './components/apoyo-profesoral/apoyo-profesoral';
 
 export const routes: Routes = [
+    {
+        path: 'apoyo_profesoral',
+        component: ApoyoProfesoral
+    },
     {
         path: '**',
         redirectTo: '',
