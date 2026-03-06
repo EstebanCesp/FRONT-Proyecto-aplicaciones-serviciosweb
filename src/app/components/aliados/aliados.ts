fe01be22d993eca36b1051ba469b6168874f5ddd
EstebanCesp <estebancespedes1127042@correo.itm.edu.co>
2026-03-05 23:16:05 -0500

    Creacion de la tabla aliados con su conexion a la api

--- src/app/components/aliados/aliados.ts
+++ src/app/components/aliados/aliados.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { aliadoService } from '../../services/aliado.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-aliados',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './aliados.html',
   styleUrl: './aliados.css',
 })
-export class Aliados {}
+export class Aliados {
+  public listaAliados:any;
+  constructor(private aliadoService : aliadoService) {}
+
+  ngOnInit(){
+    this.ObtenerAliados()
+  }
+
+  ObtenerAliados(){ 
+    let isOk: boolean;
+    let responseList : any ;
+    this.aliadoService.getAliados().subscribe(
+      {
+        next: (response) => {responseList = response; isOk=true; console.log(responseList)},
+        error: (err) => {console.error('Error al obtener datos', err); isOk=false},
+        complete: () => {
+          if(isOk){
+            this.listaAliados = responseList.datos
+          }
+        }
+      }
+    );
+  }
+}
