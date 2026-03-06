0050d389aedc735573f9cfa74faaac8af9bc9b1d
EstebanCesp <estebancespedes1127042@correo.itm.edu.co>
2026-03-06 03:12:51 -0500

    Finalizacion del componente aliados

--- src/app/components/aliados/aliados.ts
+++ src/app/components/aliados/aliados.ts
@@ -1,34 +1,97 @@
-import { Component } from '@angular/core';
-import { aliadoService } from '../../services/aliado.service';
 import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Aliado, aliadoUpdate} from '../../models/aliado';
+import { aliadoService } from '../../services/aliado.service';
 
 @Component({
   selector: 'app-aliados',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './aliados.html',
   styleUrl: './aliados.css',
 })
 export class Aliados {
-  public listaAliados:any;
-  constructor(private aliadoService : aliadoService) {}
+  public listaAliados: Aliado[] = [];
+  modoFormulario: 'crear' | 'editar' | null = null;
 
-  ngOnInit(){
-    this.ObtenerAliados()
+  formulario: Aliado = {
+    nit : 0,
+    razon_social: '',
+    nombre_contacto: '',
+    correo: '',
+    telefono: '',
+    ciudad : ''
+  };
+  constructor(
+    private aliadoService: aliadoService,
+  ) {}
+
+  ngOnInit() {
+    this.ObtenerAliados();
   }
 
-  ObtenerAliados(){ 
-    let isOk: boolean;
-    let responseList : any ;
-    this.aliadoService.getAliados().subscribe(
-      {
-        next: (response) => {responseList = response; isOk=true; console.log(responseList)},
-        error: (err) => {console.error('Error al obtener datos', err); isOk=false},
-        complete: () => {
-          if(isOk){
-            this.listaAliados = responseList.datos
-          }
-        }
+  abrirCrear(): void {
+    this.modoFormulario = 'crear';
+    this.formulario = {
+      nit : 0,
+      razon_social: '',
+      nombre_contacto: '',
+      correo: '',
+      telefono: '',
+      ciudad : ''
+    };
+  }
+
+  abrirEditar(aliado: aliadoUpdate): void {
+    this.modoFormulario = 'editar';
+    this.formulario = {
+      nit : aliado.nit??0,
+      razon_social: aliado.razon_social??'',
+      nombre_contacto: aliado.nombre_contacto??'',
+      correo: aliado.correo??'',
+      telefono: aliado.correo??'',
+      ciudad : aliado.correo??''
+    };
+  }
+
+  guardar(): void {
+    if (this.modoFormulario === 'crear') {
+      this.aliadoService.postAliado(this.formulario).subscribe(() => {
+        this.ObtenerAliados();
+        this.modoFormulario = null;
+      });
+    } else if (this.modoFormulario === 'editar') {
+      this.aliadoService.putAliado(this.formulario, this.formulario.nit).subscribe(() => {
+        this.ObtenerAliados();
+        this.modoFormulario = null;
+      });
+    }
+  }
+  
+
+  ObtenerAliados() {
+    this.aliadoService.getAliados().subscribe({
+      next: (response: any) => {
+        this.listaAliados = (response?.datos ?? (Array.isArray(response) ? response : [])) as Aliado[];
+      },
+      error: (err) => {
+        console.error('Error al obtener datos', err);
+        this.listaAliados = [];
+      },
+    });
+  }
+
+
+  cancelar(): void {
+    this.modoFormulario = null;
+  }
+
+  eliminarAliado(aliado:number){
+    this.aliadoService.deleteAliado(aliado).subscribe({
+      next: (response :any) => {
+        console.log(response)
       }
-    );
+    })
   }
+
 }
