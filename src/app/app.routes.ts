5b845eb321affe7b3c5494602c2029d404953092
EstebanCesp <estebancespedes1127042@correo.itm.edu.co>
2026-04-09 14:09:18 -0500

    creacion e implementacion de la pantalla de crud a docentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { ApoyoProfesoral } from './components/apoyo-profesoral/apoyo-profesoral';
 import { Aliados } from './components/aliados/aliados';
 import { BecaComponent } from './components/beca/beca.components';
-import { DocenteComponent } from './components/docente.component/docente.component';
+
 import { DocenteDepartamentoComponent } from './components/docente-departamento.component/docente-departamento.component';
 import { AlianzaComponent } from './components/alianza.component/alianza.component';
+import { DocenteComponent } from './components/docente.component/docente.component';
 
 export const routes: Routes = [
     {
