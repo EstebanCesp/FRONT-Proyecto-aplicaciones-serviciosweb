27456e11ef28d49419e981d8600897edbfd06da8
David Arrieta <davidarrieta1212@gmail.com>
2026-04-09 23:44:51 -0500

    feat: eliminar-angular-material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListen
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { MatDialogModule } from '@angular/material/dialog';
+
 
 import { routes } from './app.routes';
 
@@ -11,7 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
     provideAnimations(),
-    importProvidersFrom(MatDialogModule),
     provideRouter(routes)
   ]
 };
